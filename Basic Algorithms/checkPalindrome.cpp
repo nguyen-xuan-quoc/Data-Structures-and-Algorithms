15fc0947429272c4fbe1996f52a92fa236b4060e
A_cup_of_coffee <xuanquoc1352002@gmail.com>
2022-03-31 01:13:06 +0700

    init

--- Basic Algorithms/checkPalindrome.cpp
+++ Basic Algorithms/checkPalindrome.cpp
@@ -3,3 +3,17 @@
 
 using namespace std;
 
+string s;
+bool checkPalindrome(string s) {
+    string re = "";
+    for(int i = s.length() -1; i  >= 0; i --){
+        re += s[i];
+    }
+    return s == re;
+}
+
+int main(){
+    getline(cin, s);
+    cout << checkPalindrome(s) << endl;
+    return 0;
+}
